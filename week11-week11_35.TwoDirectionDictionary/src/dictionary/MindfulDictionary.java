9842c30b0e91afb23043acb39de5546485c7dde0
Kevin Kerin <kevin.kerin@gmail.com>
2019-10-30 12:47:25 +0000

    New commit

---
+++ week11-week11_35.TwoDirectionDictionary/src/dictionary/MindfulDictionary.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dictionary;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+/**
+ *
+ * @author kevinkerin
+ */
+public class MindfulDictionary {
+
+    private Map<String, String> dictionary;
+    private String file;
+
+    public MindfulDictionary() {
+        dictionary = new HashMap<String, String>();
+    }
+
+    public MindfulDictionary(String file) {
+        dictionary = new HashMap<String, String>();
+        this.file = file;
+    }
+
+    public boolean load() {
+        try {
+            FileReader fr = new FileReader(file);
+            Scanner fileReader = new Scanner(new File(file));
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                String[] parts = line.split(":");   // the line is split at :
+                dictionary.put(parts[0], parts[1]); // the part of the line after :
+                //dictionary.put(parts[0], parts[1]);
+            }
+            return true;
+        } catch (FileNotFoundException e) {
+            return false;
+        }
+    }
+    
+    public boolean save(){
+        try {
+            FileWriter fw = new FileWriter(file);
+            for (Entry<String, String> entry : dictionary.entrySet()){
+                fw.write(entry.getKey() + ":" + entry.getValue() + "\n");
+            }
+            fw.close();
+            return true;
+        } catch (IOException e){
+            return false;
+        }
+    }
+
+    public void add(String word, String translation) {
+        boolean contains = false;
+        for (String str : dictionary.keySet()) {
+            if (str.equals(word)) {
+                contains = true;
+            }
+        }
+        if (!contains) {
+            dictionary.put(word, translation);
+        }
+    }
+
+    public String translate(String word) {
+        for (Map.Entry<String, String> entry : dictionary.entrySet()) {
+            if (entry.getKey().equalsIgnoreCase(word)) {
+                return entry.getValue();
+            } else if (entry.getValue().equalsIgnoreCase(word)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    public void remove(String word) {
+        String removeMe = null;
+        for (Map.Entry<String, String> entry : dictionary.entrySet()) {
+            if (entry.getKey().equalsIgnoreCase(word)) {
+                removeMe = entry.getKey();
+            } else if (entry.getValue().equalsIgnoreCase(word)) {
+                removeMe = entry.getKey();
+            }
+        }
+        dictionary.remove(removeMe);
+    }
+
+}
