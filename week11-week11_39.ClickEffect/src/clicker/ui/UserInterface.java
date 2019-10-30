592e126869f6f330433339ff89fd94362e1bb84b
Kevin Kerin <kevin.kerin@gmail.com>
2019-10-30 16:27:20 +0000

    New commit

---
+++ week11-week11_39.ClickEffect/src/clicker/ui/UserInterface.java
@@ -0,0 +1,48 @@
+package clicker.ui;
+
+import clicker.applicationlogic.Calculator;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.*;
+
+public class UserInterface implements Runnable {
+    
+    private JFrame frame;
+    private Calculator calculator;
+    
+    public UserInterface(Calculator calculator){
+        this.calculator = calculator;
+    }
+
+
+    @Override
+    public void run() {
+        frame = new JFrame("Click Effect");
+        frame.setPreferredSize(new Dimension(200, 100));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        createComponents(frame.getContentPane());
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void createComponents(Container container) {
+        GridLayout gl = new GridLayout(3, 1);
+        container.setLayout(gl);
+        
+        JLabel label = new JLabel("0");
+        JButton button = new JButton("Click!");
+        
+        ClickListener cl = new ClickListener(calculator, label);
+        button.addActionListener(cl);
+        
+        container.add(label);
+        container.add(button);
+    }
+
+    public JFrame getFrame() {
+        return frame;
+    }
+}
